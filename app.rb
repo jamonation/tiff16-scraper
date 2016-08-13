d6cd16ba60c413b0c2eddb61f74047780d4cbce5
James Wilkinson <james@jerw.ca>
2016-08-12 20:03:07 -0400

    Adds looping of film urls, writes to a json file

--- app.rb
+++ app.rb
@@ -1,23 +1,32 @@
 require 'nokogiri'
 require 'open-uri'
+require 'json'
 
-tiffDOM = Nokogiri::HTML(open("http://tiff.net/films/fluffy"), nil, 'utf-8')
-film = Hash.new
-film["name"] = tiffDOM.css("body.film #wrap .container h1").text || ""
-film["director"] = tiffDOM.css("#director .credit-content").text || ""
-film["countries"] = tiffDOM.css("span.quick-info .countries").text || ""
-film["runtime"] = tiffDOM.css("span.quick-info .runtime").text || ""
-film["premiere"] = tiffDOM.css("span.quick-info .premiere").text || ""
-film["year"] = tiffDOM.css("span.quick-info .year").text || ""
-film["language"] = tiffDOM.css("span.quick-info .language").text || ""
-film["pitch"] = tiffDOM.css(".pitch").text || ""
-film["production"] = tiffDOM.css("#productionCompany .credit-content").text || ""
-film["producers"] = tiffDOM.css("#producers .credit-content").text || ""
-film["screenplay"] = tiffDOM.css("#screenplay .credit-content").text || ""
-film["cinematographers"] = tiffDOM.css("#cinematographers .credit-content").text || ""
-film["editors"] = tiffDOM.css("#editors .credit-content").text || ""
-film["score"] = tiffDOM.css("#originalScore .credit-content").text || ""
-film["sound"] = tiffDOM.css("#sound .credit-content").text || ""
-film["cast"] = tiffDOM.css("#cast .credit-content").text || ""
+urls = ["http://tiff.net/films/anna/", "http://tiff.net/films/fluffy"]
+films = Array.new
 
-puts film.inspect
+for url in urls do
+  tiffDOM = Nokogiri::HTML(open(url), nil, 'utf-8')
+  film = Hash.new
+  film["name"] = tiffDOM.css("body.film #wrap .container h1").text || ""
+  film["director"] = tiffDOM.css("#director .credit-content").text || ""
+  film["countries"] = tiffDOM.css("span.quick-info .countries").text || ""
+  film["runtime"] = tiffDOM.css("span.quick-info .runtime").text || ""
+  film["premiere"] = tiffDOM.css("span.quick-info .premiere").text || ""
+  film["year"] = tiffDOM.css("span.quick-info .year").text || ""
+  film["language"] = tiffDOM.css("span.quick-info .language").text || ""
+  film["pitch"] = tiffDOM.css(".pitch").text || ""
+  film["production"] = tiffDOM.css("#productionCompany .credit-content").text || ""
+  film["producers"] = tiffDOM.css("#producers .credit-content").text || ""
+  film["screenplay"] = tiffDOM.css("#screenplay .credit-content").text || ""
+  film["cinematographers"] = tiffDOM.css("#cinematographers .credit-content").text || ""
+  film["editors"] = tiffDOM.css("#editors .credit-content").text || ""
+  film["score"] = tiffDOM.css("#originalScore .credit-content").text || ""
+  film["sound"] = tiffDOM.css("#sound .credit-content").text || ""
+  film["cast"] = tiffDOM.css("#cast .credit-content").text || ""
+  films.push(film)
+end
+
+File.open("films/films.json","w") do |f|
+  f.write(JSON.pretty_generate(films))
+end
